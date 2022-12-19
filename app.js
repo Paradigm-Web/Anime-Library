c46da6f7e135facada5e0c78b53223ac56cbd32f
Paradigm-Web <120433682+Paradigm-Web@users.noreply.github.com>
2022-12-20 00:05:32 +1300

    Small changes to styles

--- app.js
+++ app.js
@@ -17,12 +17,14 @@ theme.addEventListener('click', () => {
     document.body.classList.toggle('dark');
 });
 
-const search_btn = document.querySelector('.search-icon');
-search_btn.addEventListener('click', () => {
-    document.querySelector('.search-box').classList.toggle('search-box-open');
-});
-
 const search_input = document.querySelector('#search-input');
 search_input.addEventListener('blur', () => {
     document.querySelector('.search-box').classList.toggle('search-box-open');
 });
+
+const search_btn = document.querySelector('.search-icon');
+search_btn.addEventListener('click', () => {
+    document.querySelector('.search-box').classList.toggle('search-box-open');
+    search_input.focus();
+});
+
