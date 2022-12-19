f260befe8ab7ac2838b32c729a978eaac5cad0a2
Paradigm-Web <120433682+Paradigm-Web@users.noreply.github.com>
2022-12-19 20:13:04 +1300

    Basics for the header bar

--- app.js
+++ app.js
@@ -7,3 +7,22 @@ function Book() {
 function addBookToLibrary() {
     
 }
+
+
+const theme = document.querySelector('.theme > img');
+theme.addEventListener('click', () => {
+    theme.classList.toggle('light');
+    theme.classList.toggle('dark');
+    document.body.classList.toggle('light');
+    document.body.classList.toggle('dark');
+});
+
+const search_btn = document.querySelector('.search-icon');
+search_btn.addEventListener('click', () => {
+    document.querySelector('.search-box').classList.toggle('search-box-open');
+});
+
+const search_input = document.querySelector('#search-input');
+search_input.addEventListener('blur', () => {
+    document.querySelector('.search-box').classList.toggle('search-box-open');
+});
