20fd737013524f529c65be8516b6b95866a719f0
Paradigm-Web <120433682+Paradigm-Web@users.noreply.github.com>
2022-12-26 22:36:17 +1300

    slightly messy still
    On my process to rework the old messy code, I've made my javascript and modal code pretty messy so I will rework that another time with simpler code and less hardcoded parts. For now I have the second menu with left controls working, and the settings slideout

--- app.js
+++ app.js
@@ -28,6 +28,65 @@ search_btn.addEventListener('click', () => {
     search_input.focus();
 });
 
+function toggleModal(modal, wrapper) {
+    wrapper.classList.toggle('active');
+    modal.classList.toggle('active');
+    overlay.classList.toggle('open');
+}
+
+const add_btn = document.querySelector('#add');
+const upload_btn = document.querySelector('#upload');
+const share_btn = document.querySelector('#share');
+
+const modals = document.querySelectorAll('.modal');
+const add_wrapper = document.querySelector('#add-wrapper')
+const add_modal = document.querySelector('#add-card-modal');
+const upload_modal = document.querySelector('#upload-list-modal');
+const upload_wrapper = document.querySelector('#upload-wrapper')
+const share_modal = document.querySelector('#share-list-modal');
+const share_wrapper = document.querySelector('#share-wrapper')
+const overlay = document.querySelector('.overlay');
+
+add_btn.addEventListener('click', () => {
+    toggleModal(add_modal, add_wrapper);
+});
+const close_add = document.querySelector('#close-add');
+close_add.addEventListener('click', () => {
+    toggleModal(add_modal, add_wrapper);
+})
+
+upload_btn.addEventListener('click', () => {
+    toggleModal(upload_modal, upload_wrapper);
+});
+const close_upload = document.querySelector('#close-upload');
+close_upload.addEventListener('click', () => {
+    toggleModal(upload_modal, upload_wrapper);
+})
+
+share_btn.addEventListener('click', () => {
+    toggleModal(share_modal, share_wrapper);
+});
+const close_share = document.querySelector('#close-share');
+close_share.addEventListener('click', () => {
+    toggleModal(share_modal, share_wrapper);
+})
+
+const settings_wrapper = document.querySelector('.settings-wrapper');
+const settings_btn = document.querySelector('#settings > i');
+function openSettings() {
+    settings_wrapper.classList.toggle('settings-open');
+    document.querySelector('#settings').classList.toggle('settings-clicked');
+}
+settings_btn.addEventListener('click', () => {
+    openSettings();
+})
+
+
+
+
+
+
+
 // const s_wrapper = document.querySelector('.settings-wrapper');
 // // const setting_caret = document.querySelector('.settingcaret')
 // const settings_btn = document.querySelector('.sbtn');
